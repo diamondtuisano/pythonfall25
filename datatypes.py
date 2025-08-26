31765d219c43252e3debe914838fd7e6a81ced03
diamondtuisano <dtuisano@students.mchenry.edu>
2025-08-26 11:24:35 -0500

    Add files via upload
    data types assignment

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+"""
+
+Copy the sample program into your Python editor. Run the program and 
+observe the output. Change the variable values, test the program, 
+and submit your file. 
+
+"""
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 15)
+
+# Integer
+y = -45
+print("Integer:", y)
+
+# Float
+z = 3.14
+print("Float:", z)
+
+# String
+name = "Diamond"
+print("String:", name)
+
+# Boolean
+is_active = True
+is_not_active = False
+print("Boolean:", is_not_active)
