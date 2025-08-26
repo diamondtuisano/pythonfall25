50f8f91747b2342995601878266cdba14617de6b
diamondtuisano <dtuisano@students.mchenry.edu>
2025-08-26 11:00:28 -0500

    Add files via upload
    chiropractor joke

---
+++ joke.py
@@ -0,0 +1,19 @@
+"""
+Create an original knock-knock joke or find one you like online and use it when you write your program. 
+Submit the .py file. Grading is complete/incomplete, and you may resubmit within a week of when the submission is graded. 
+Include at least one comment (starts with # ) to describe the program's purpose.
+
+About this Assignment:
+This assignment should take approximately 30 - 45 minutes of study and 10 minutes of coding. 
+Upload your .py file to Canvas.
+See grading details in the rubric below.
+
+"""
+
+#Joke Assignment using the print function
+
+print("Person 1: Have I ever told you that joke about the chiropractor?")
+print("Person 2: No.")
+print("Person 1: I'm pretty sure I did.")
+print("Person 2: I don't remember.")
+print("Person 1: Yeah, I did. I told it to you...about a week back.")
