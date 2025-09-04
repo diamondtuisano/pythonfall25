499d60ac2c3e02b14d9e85a921a71a5432e631fb
diamondtuisano <dtuisano@students.mchenry.edu>
2025-09-04 10:57:03 -0500

    Add files via upload
    comments (miles per gallon, tank size, distance)

---
+++ comments.py
@@ -0,0 +1,25 @@
+"""
+tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+
+Type the above code into Visual Studio Code.
+Add a minimum of 4 comments to the above Python program to make it more understandable.
+
+"""
+
+#define the tank size of the car
+tank_size = 15 #gallons
+
+#define the distance the car is going
+distance = 600 #miles
+
+#calculate the miles per gallon being used
+mpg = distance / tank_size
+
+#print the results
+print("Miles per gallon:", mpg) #should be an integer or float
