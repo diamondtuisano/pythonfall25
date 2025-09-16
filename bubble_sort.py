5468519355896f0f314f0d1a0eb361542de9f37c
diamondtuisano <dtuisano@students.mchenry.edu>
2025-09-16 11:43:53 -0500

    Add files via upload
    bubble_first_attempt

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+"""
+
+Prompt the user to enter five names.
+Store the names in a list.
+Sort the list using the Bubble Sort algorithm.
+Reverse the sorted list using a Python list method.
+Print both the sorted and reversed lists.
+
+"""
+
+#prompt the user to enter five names
+str(print(input(f"Enter five names: ")))
+
+names = [input]
+
+print(names)
