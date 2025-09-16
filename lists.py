e392041c275266603773f429bf5f36c06abdb9cb
diamondtuisano <dtuisano@students.mchenry.edu>
2025-09-16 11:44:30 -0500

    Add files via upload
    lists_days_of_the_week

---
+++ lists.py
@@ -0,0 +1,35 @@
+"""
+
+Create a List: Start by creating a list named days that includes all seven days of 
+the week.
+Initialize an Empty List: Create an empty list called steps to store the number of 
+steps taken each day.
+User Input: Using a loop, ask the user to enter the number of steps they took for 
+each day.
+Store Steps: Append the user's input to the steps list.
+Display Daily Steps: Show the user the steps recorded for each day.
+Total Steps: Calculate and display the total number of steps taken in the week.
+Average Steps: Calculate and display the average steps.
+
+"""
+
+#days of the week
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+#empty list
+my_steps = []
+
+#range and input from user
+for day in days:
+   in_steps = int(input(f"How many steps did you take on {day}? "))
+   my_steps = my_steps.append(in_steps) #store steps
+   print(f"{day} you walked {in_steps} steps.")
+
+for steps in my_steps:
+    print(steps)
+    total = total + steps
+
+#pulls out of for loop
+print(f"Total steps: {total}")
+
+print(f"Average steps: {total / len(steps): .2f}")
