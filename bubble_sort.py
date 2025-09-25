248ad9e04f748481a88d6e8d4232430de33a59b4
diamondtuisano <dtuisano@students.mchenry.edu>
2025-09-25 11:59:07 -0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,6 +11,17 @@ Print both the sorted and reversed lists.
 #prompt the user to enter five names
 str(print(input(f"Enter five names: ")))
 
-names = [input]
+for range i in names
 
+
+
+#empty list
+names = [input]
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(len(names) -1):
+        if names[i] > names[i + 1]:
+            names[i], names[i + 1] = names [i +1], names[i]
+            swapped = True
 print(names)
