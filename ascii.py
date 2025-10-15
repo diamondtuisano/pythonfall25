3ebd0c7bc573a73b9a6546cd4974e05632cc0ff4
diamondtuisano <dtuisano@students.mchenry.edu>
2025-10-15 18:29:03 -0500

    Add files via upload
    ascii

--- ascii.py
+++ ascii.py
@@ -35,25 +35,45 @@ a. Upload your script to GitHub.
 b. Submit a link to your repository.
 
 """
+def chr_ascii(): #define the single character ascii function
+    while True: #while loop
+        try: #account for errors within loop
+            user_input1 = input(f"Please enter a single character: ") #user input1 variable defined
+            
+            if len(user_input1) == 1: #if user inputs a character length equal to 1
+                ascii_value = ord(user_input1) #converts user's single-character input to ascii integer
+                print(f"The ASCII integer value of {user_input1} is: {ascii_value}.") #prints correct message
+                return #exits function and loop after valid input
+            else: #if user doesn't input only one character
+                print(f"Invalid. Please enter a single character: ") #print error message
+
+        except TypeError as e: #if user input does not match the ability of the function
+            print(f"A TypeError has occurred: {e}.") #print TypeError 
+        
+def int_ascii(): #define the integer value with corresponding ascii function
+    while True: #while loop
+        userinput2 = input(f"Please enter an ASCII integer value between 32-127: ") #user input2 variable defined
+
+        try: #account for errors within loop
+            ascii_value = int(userinput2) #converts user's integer input to ascii character
+
+            if 32 <= ascii_value <= 127: #userinput2 ascii_value range between 32-127
+                character = chr(ascii_value) #character function 'chr' gets ascii's corresponding character value
+                print(f"The character for ASCII value {ascii_value} is {character}.") #print character value
+                return #exits function and loop after valid input
+            else: #otherwise
+                print(f"Invalid. Please enter a value between 32-127: ") #print error message
+        
+        except ValueError as e: #if user input has correct data type but inappropriate value
+            print(f"A ValueError occurred: {e}.") #prints ValueError
 
 def main(): #define main
-    try: #test the block of code for errors
-        chr = input(f"Please enter a single character: ") #character input as a variable
-        while len(chr) != 1: #loop w/while statement, if length of character input is not 1
-            print(f"That is not a good input.") #print character error statement
-            chr = input(f"Please enter a single character: ") #prompts user to enter a single character
-        ascii_value = ord(chr) #convert to ascii value
-        print(f"The value of {chr} is {ascii_value}.") #display result; print ascii value to the user
+    print(f"[Convert ASCII single-charcter value to integer] ") #print first step of assignment
+    chr_ascii() #returns 'ascii_value'; single character as a number
 
-        i = int(input(f"Please enter an ASCII value between 32-127: ")) #prompt user to enter an ascii value
-        i = range(32,128) #defines i as variable within the range 32-127
-        while i in range: #loop for the values in range 32-127
-            print(f"The value of {i} is {ascii_value}.") #print the value
-        else: #otherwise 
-            print(f"That is not a valid number.") #print int error value statement
-            int = int(input(f"Please enter a number between 32-127: ")) #prompts user to enter an int 32-127
+    print(f"[Convert integer ASCII value to character]") #print second step of assignment
+    int_ascii() #returns 'character'; integer as a single character
 
-    except Exception as e:
-        print(e)
+    print(f"Thanks for using the ASCII converter!") #print exit message
 
 main() #call main
